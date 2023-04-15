61b3e08a91fce2ead574ae5908ab5c18b42941c1
TamTVH <billybu96969@gmail.com>
2023-04-16 04:09:39 +0700

    m

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
+
+    private static int sumValues(Map<String, Integer> map){
+        Set<String> set = map.keySet();
+        int sum = 0;
+        for(String key : set){
+            sum += map.get(key);
+        }
+
+        return sum;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+
+        System.out.println("Size of map is:- " + map.size());
+
+        System.out.println(map);
+
+        if (map.containsKey("one")) {
+
+            Integer a = map.get("one");
+
+            System.out.println("value for key" + " \"one\" is: " + a);
+        }
+
+        map.computeIfAbsent("four", k -> sumValues(map));
+
+        System.out.println(map);
     }
 }
